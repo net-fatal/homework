5b706e40d8f574013b532503f8734aaa278d34fd
Ulugbek <ulugbek.kasymov@gmail.com>
2023-02-03 20:10:41 +0300

    add Задача4

---
+++ C#_example4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите 3 числа:");
+            int num_1 = int.Parse(Console.ReadLine());
+            int num_2 = int.Parse(Console.ReadLine());
+            int num_3 = int.Parse(Console.ReadLine());
+
+            int max = num_1;
+
+            if (num_2 > max)
+            {
+                max = num_2;
+            }
+
+            if (num_3 > max)
+            {
+                max = num_3;
+            }
+
+            Console.WriteLine($"Максимальное число {max}");
--- C#_example4/obj/Debug/net6.0/C#_example4.AssemblyInfo.cs
+++ C#_example4/obj/Debug/net6.0/C#_example4.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("C#_example1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("C#_example4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("C#_example1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("C#_example1")]
+[assembly: System.Reflection.AssemblyProductAttribute("C#_example4")]
+[assembly: System.Reflection.AssemblyTitleAttribute("C#_example4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
